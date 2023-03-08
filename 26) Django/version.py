2b8426ebf69df2d8bd8e642383caace6cfbe6037
Arexihul <66669585+Arexihul@users.noreply.github.com>
2023-03-08 17:44:04 +0300

    Add files via upload

---
+++ 26) Django/version.py
@@ -0,0 +1,21 @@
+import django
+print(django.get_version())
+
+# django-admin --version
+
+# yüklü paketler
+# pip freeze
+
+# django-admin help
+
+# django-admin startproject
+
+# cd catalog
+
+# python manage.py help
+
+# python manage.py runserver
+
+# python manage.py startapp pages(app name)
+
+# 'pages.apps.PagesConfig' add this to INSTALLED_APPS on settings.py
